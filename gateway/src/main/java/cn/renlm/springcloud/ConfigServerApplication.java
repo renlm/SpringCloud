4acfc931560d9a8413f96c3b588906a253cbbd54
renlm <renlmer@qq.com>
2022-11-24 13:00:43 +0800

    gw

---
+++ gateway/src/main/java/cn/renlm/springcloud/ConfigServerApplication.java
@@ -0,0 +1,20 @@
+package cn.renlm.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * 启动类
+ * 
+ * @author Renlm
+ *
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerApplication {
+	public static void main(String[] args) {
+		SpringApplication springApplication = new SpringApplication(ConfigServerApplication.class);
+		springApplication.run(args);
+	}
+}
