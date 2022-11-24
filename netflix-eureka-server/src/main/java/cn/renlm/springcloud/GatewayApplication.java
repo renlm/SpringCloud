8a4ca73af90317aca80c38e5e0b8151f0a612cad
renlm <renlmer@qq.com>
2022-11-24 20:03:30 +0800

    sc

---
+++ netflix-eureka-server/src/main/java/cn/renlm/springcloud/GatewayApplication.java
@@ -0,0 +1,18 @@
+package cn.renlm.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ * 
+ * @author Renlm
+ *
+ */
+@SpringBootApplication
+public class GatewayApplication {
+	public static void main(String[] args) {
+		SpringApplication springApplication = new SpringApplication(GatewayApplication.class);
+		springApplication.run(args);
+	}
+}
