8935038011bc495b83b3df260b06ad34dda89952
任黎明(10021235) <renlm-a@glodon.com>
2022-12-04 16:20:54 +0800

    sc

--- config-server/src/main/java/cn/renlm/springcloud/config/WebSecurityConfig.java
+++ config-server/src/main/java/cn/renlm/springcloud/config/WebSecurityConfig.java
@@ -12,7 +12,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
  *
  */
 @EnableWebSecurity
-public class ConfigServerSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
