02911e8852d7ccf0599d3fa262ba8a5ecb62920b
任黎明(10021235) <renlm-a@glodon.com>
2022-12-06 22:50:33 +0800

    sc

--- gateway/src/main/java/cn/renlm/springcloud/config/WebFluxSecurityConfig.java
+++ gateway/src/main/java/cn/renlm/springcloud/config/WebFluxSecurityConfig.java
@@ -34,27 +34,29 @@ public class WebFluxSecurityConfig {
 	@Bean
 	public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http,
 			SecurityIgnoreProperties securityIgnoreProperties) {
-		http.authorizeExchange().matchers(new ServerWebExchangeMatcher() {
+		http.authorizeExchange()
+			.matchers(new ServerWebExchangeMatcher() {
 			
-			@Override
-			public Mono<MatchResult> matches(ServerWebExchange exchange) {
-				List<String> whites = securityIgnoreProperties.getWhites();
-				if (whites == null) {
+				@Override
+				public Mono<MatchResult> matches(ServerWebExchange exchange) {
+					List<String> whites = securityIgnoreProperties.getWhites();
+					if (whites == null) {
+						return MatchResult.notMatch();
+					}
+					for (String uri : whites) {
+						if (antPathMatcher.match(uri, exchange.getRequest().getURI().getPath())) {
+							return MatchResult.match();
+						}
+					}
 					return MatchResult.notMatch();
 				}
-				for (String uri : whites) {
-					if (antPathMatcher.match(uri, exchange.getRequest().getURI().getPath())) {
-						return MatchResult.match();
-					}
-				}
-				return MatchResult.notMatch();
-			}
 			
-		}).permitAll()
+			}).permitAll()
 		.pathMatchers(HttpMethod.OPTIONS).permitAll()
 		.anyExchange().authenticated();
 		http.oauth2ResourceServer().jwt();
 		http.oauth2Login(withDefaults());
+		http.cors().and().csrf().disable();
 		return http.build();
 	}
 
