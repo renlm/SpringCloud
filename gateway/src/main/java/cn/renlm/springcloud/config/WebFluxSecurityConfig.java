23ad4e5327b6b867c24feff071356511e17b22f3
任黎明(10021235) <renlm-a@glodon.com>
2022-12-06 16:25:19 +0800

    sc

--- gateway/src/main/java/cn/renlm/springcloud/config/WebFluxSecurityConfig.java
+++ gateway/src/main/java/cn/renlm/springcloud/config/WebFluxSecurityConfig.java
@@ -1,9 +1,15 @@
 package cn.renlm.springcloud.config;
 
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties.Opaquetoken;
+import org.springframework.boot.autoconfigure.web.client.RestTemplateAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.server.resource.introspection.NimbusReactiveOpaqueTokenIntrospector;
+import org.springframework.security.oauth2.server.resource.introspection.ReactiveOpaqueTokenIntrospector;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.session.data.redis.config.annotation.web.server.EnableRedisWebSession;
 
@@ -16,6 +22,7 @@ import org.springframework.session.data.redis.config.annotation.web.server.Enabl
 @EnableWebFluxSecurity
 @Configuration(proxyBeanMethods = false)
 @EnableRedisWebSession(redisNamespace = "gateway:session")
+@ImportAutoConfiguration({ RestTemplateAutoConfiguration.class })
 public class WebFluxSecurityConfig {
 
 	@Bean
@@ -23,6 +30,14 @@ public class WebFluxSecurityConfig {
 		http.authorizeExchange().anyExchange().authenticated();
 		http.oauth2ResourceServer().opaqueToken();
 		return http.build();
+	}
+
+	@Bean
+	public ReactiveOpaqueTokenIntrospector introspector(OAuth2ResourceServerProperties properties) {
+		Opaquetoken ot = properties.getOpaquetoken();
+		NimbusReactiveOpaqueTokenIntrospector delegate = new NimbusReactiveOpaqueTokenIntrospector(
+				ot.getIntrospectionUri(), ot.getClientId(), ot.getClientId());
+		return token -> delegate.introspect(token);
 	}
 
 }
