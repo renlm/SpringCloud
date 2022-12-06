240007cb86b2cdd80f66e82d72849bcc65f02ad8
任黎明(10021235) <renlm-a@glodon.com>
2022-12-06 17:53:20 +0800

    sc

--- gateway/src/main/java/cn/renlm/springcloud/config/WebFluxSecurityConfig.java
+++ gateway/src/main/java/cn/renlm/springcloud/config/WebFluxSecurityConfig.java
@@ -1,5 +1,7 @@
 package cn.renlm.springcloud.config;
 
+import java.time.Instant;
+
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties.Opaquetoken;
@@ -8,10 +10,22 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.security.oauth2.client.userinfo.DefaultReactiveOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.ReactiveOAuth2UserService;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
+import org.springframework.security.oauth2.core.OAuth2AccessToken.TokenType;
+import org.springframework.security.oauth2.core.OAuth2AuthenticatedPrincipal;
+import org.springframework.security.oauth2.core.OAuth2TokenIntrospectionClaimNames;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.oauth2.server.resource.introspection.NimbusReactiveOpaqueTokenIntrospector;
 import org.springframework.security.oauth2.server.resource.introspection.ReactiveOpaqueTokenIntrospector;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.session.data.redis.config.annotation.web.server.EnableRedisWebSession;
+
+import reactor.core.publisher.Mono;
 
 /**
  * Security 配置
@@ -33,11 +47,22 @@ public class WebFluxSecurityConfig {
 	}
 
 	@Bean
-	public ReactiveOpaqueTokenIntrospector introspector(OAuth2ResourceServerProperties properties) {
+	public ReactiveOpaqueTokenIntrospector introspector(ReactiveClientRegistrationRepository repository,
+			OAuth2ResourceServerProperties properties) {
 		Opaquetoken ot = properties.getOpaquetoken();
 		NimbusReactiveOpaqueTokenIntrospector delegate = new NimbusReactiveOpaqueTokenIntrospector(
 				ot.getIntrospectionUri(), ot.getClientId(), ot.getClientId());
-		return token -> delegate.introspect(token);
+		ReactiveOAuth2UserService<OAuth2UserRequest, OAuth2User> oauth2UserService = new DefaultReactiveOAuth2UserService();
+		return token -> {
+			return Mono.zip(delegate.introspect(token), repository.findByRegistrationId(ot.getClientId())).map(t -> {
+				OAuth2AuthenticatedPrincipal authorized = t.getT1();
+				ClientRegistration clientRegistration = t.getT2();
+				Instant issuedAt = authorized.getAttribute(OAuth2TokenIntrospectionClaimNames.IAT);
+				Instant expiresAt = authorized.getAttribute(OAuth2TokenIntrospectionClaimNames.EXP);
+				OAuth2AccessToken accessToken = new OAuth2AccessToken(TokenType.BEARER, token, issuedAt, expiresAt);
+				return new OAuth2UserRequest(clientRegistration, accessToken);
+			}).flatMap(oauth2UserService::loadUser);
+		};
 	}
 
 }
