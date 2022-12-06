1afe916ffe8b52ea23a3df8cbf5f48703163659b
任黎明(10021235) <renlm-a@glodon.com>
2022-12-06 21:01:40 +0800

    sc

--- gateway/src/main/java/cn/renlm/springcloud/config/WebFluxSecurityConfig.java
+++ gateway/src/main/java/cn/renlm/springcloud/config/WebFluxSecurityConfig.java
@@ -2,12 +2,20 @@ package cn.renlm.springcloud.config;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatcher;
 import org.springframework.session.data.redis.config.annotation.web.server.EnableRedisWebSession;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.web.server.ServerWebExchange;
+
+import cn.renlm.springcloud.properties.SecurityIgnoreProperties;
+import reactor.core.publisher.Mono;
 
 /**
  * Security 配置
@@ -20,9 +28,26 @@ import org.springframework.session.data.redis.config.annotation.web.server.Enabl
 @EnableRedisWebSession(redisNamespace = "gateway:session")
 public class WebFluxSecurityConfig {
 
+	private final AntPathMatcher antPathMatcher = new AntPathMatcher();
+
 	@Bean
-	public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
-		http.authorizeExchange().anyExchange().authenticated();
+	public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http,
+			SecurityIgnoreProperties securityIgnoreProperties) {
+		http.authorizeExchange().matchers(new ServerWebExchangeMatcher() {
+			@Override
+			public Mono<MatchResult> matches(ServerWebExchange exchange) {
+				List<String> whites = securityIgnoreProperties.getWhites();
+				if (whites == null) {
+					return MatchResult.notMatch();
+				}
+				for (String uri : whites) {
+					if (antPathMatcher.match(uri, exchange.getRequest().getURI().getPath())) {
+						return MatchResult.match();
+					}
+				}
+				return MatchResult.notMatch();
+			}
+		}).permitAll().anyExchange().authenticated();
 		http.oauth2ResourceServer().jwt();
 		http.oauth2Login(withDefaults());
 		return http.build();
---
+++ gateway/src/main/java/cn/renlm/springcloud/properties/SecurityIgnoreProperties.java
@@ -0,0 +1,25 @@
+package cn.renlm.springcloud.properties;
+
+import java.util.List;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * 访问白名单
+ * 
+ * @author Renlm
+ *
+ */
+@Getter
+@Setter
+@Component
+@ConfigurationProperties("security.ignore")
+public class SecurityIgnoreProperties {
+
+	private List<String> whites;
+
+}
