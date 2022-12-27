a2951f4030499f5687ad13ee6803b1d2d1007be5
任黎明(10021235) <renlm-a@glodon.com>
2022-12-27 20:49:23 +0800

    up

--- config-server/src/main/java/cn/renlm/springcloud/filter/GithubSignAuthenticationFilter.java
+++ config-server/src/main/java/cn/renlm/springcloud/filter/GithubSignAuthenticationFilter.java
@@ -4,20 +4,19 @@ import static org.springframework.security.core.context.SecurityContextHolder.ge
 
 import java.io.IOException;
 import java.util.Collections;
-import java.util.Map;
 
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-import org.springframework.web.util.ContentCachingRequestWrapper;
 
+import cn.hutool.core.io.IoUtil;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.crypto.digest.DigestUtil;
 import cn.hutool.crypto.digest.HMac;
 import cn.hutool.crypto.digest.HmacAlgorithm;
-import cn.hutool.json.JSONUtil;
 import cn.renlm.springcloud.properties.WebHookProperties;
+import cn.renlm.springcloud.wrapper.RepeatableHttpServletRequest;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -45,24 +44,22 @@ public class GithubSignAuthenticationFilter extends OncePerRequestFilter {
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
+		HttpServletRequest requestWrapper = new RepeatableHttpServletRequest(request);
 		if (HEADERS_VALUE.equals(request.getHeader(HEADERS_KEY))) {
-			ContentCachingRequestWrapper requestWrapper = new ContentCachingRequestWrapper(request);
-			byte[] bytes = requestWrapper.getContentAsByteArray();
-			String sign = this.sign(bytes, webHookProperties.getSignSecret());
+			byte[] bytes = IoUtil.readBytes(requestWrapper.getInputStream());
+			String body = new String(bytes, request.getCharacterEncoding());
+			String sign = this.sign(body, webHookProperties.getSignSecret());
 			String signature = request.getHeader("X-Hub-Signature-256");
 			log.info("sign = {}", sign);
 			log.info("signature = {}", signature);
-			String body = new String(bytes, request.getCharacterEncoding());
-			Map<String, String[]> mp = request.getParameterMap();
 			log.info("payload = {}", body);
-			log.info("getParameterMap = {}", JSONUtil.toJsonStr(mp));
 			if (StrUtil.equals(sign, signature)) {
 				Authentication token = new UsernamePasswordAuthenticationToken(HEADERS_KEY, HEADERS_VALUE,
 						Collections.emptySet());
 				getContext().setAuthentication(token);
 			}
 		}
-		filterChain.doFilter(request, response);
+		filterChain.doFilter(requestWrapper, response);
 	}
 
 	/**
@@ -72,7 +69,7 @@ public class GithubSignAuthenticationFilter extends OncePerRequestFilter {
 	 * @param secret
 	 * @return
 	 */
-	private String sign(byte[] payload, String secret) {
+	private String sign(String payload, String secret) {
 		HMac hMac = DigestUtil.hmac(HmacAlgorithm.HmacSHA256, secret.getBytes());
 		return "sha256=" + hMac.digestHex(payload);
 	}
---
+++ config-server/src/main/java/cn/renlm/springcloud/wrapper/RepeatableHttpServletRequest.java
@@ -0,0 +1,87 @@
+package cn.renlm.springcloud.wrapper;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import cn.hutool.core.io.IORuntimeException;
+import cn.hutool.core.io.IoUtil;
+import jakarta.servlet.ReadListener;
+import jakarta.servlet.ServletInputStream;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequestWrapper;
+
+public class RepeatableHttpServletRequest extends HttpServletRequestWrapper {
+
+	private final byte[] bytes;
+
+	public RepeatableHttpServletRequest(HttpServletRequest request) throws IORuntimeException, IOException {
+		super(request);
+		bytes = IoUtil.readBytes(request.getInputStream());
+	}
+
+	@Override
+	public ServletInputStream getInputStream() throws IOException {
+		return new ServletInputStream() {
+			private int lastIndexRetrieved = -1;
+			private ReadListener readListener = null;
+
+			@Override
+			public boolean isFinished() {
+				return (lastIndexRetrieved == bytes.length - 1);
+			}
+
+			@Override
+			public boolean isReady() {
+				return isFinished();
+			}
+
+			@Override
+			public void setReadListener(ReadListener readListener) {
+				this.readListener = readListener;
+				if (!isFinished()) {
+					try {
+						readListener.onDataAvailable();
+					} catch (IOException e) {
+						readListener.onError(e);
+					}
+				} else {
+					try {
+						readListener.onAllDataRead();
+					} catch (IOException e) {
+						readListener.onError(e);
+					}
+				}
+			}
+
+			@Override
+			public int read() throws IOException {
+				int i;
+				if (!isFinished()) {
+					i = bytes[lastIndexRetrieved + 1];
+					lastIndexRetrieved++;
+					if (isFinished() && (readListener != null)) {
+						try {
+							readListener.onAllDataRead();
+						} catch (IOException ex) {
+							readListener.onError(ex);
+							throw ex;
+						}
+					}
+					return i;
+				} else {
+					return -1;
+				}
+			}
+		};
+	}
+
+	@Override
+	public BufferedReader getReader() throws IOException {
+		ByteArrayInputStream is = new ByteArrayInputStream(bytes);
+		BufferedReader temp = new BufferedReader(new InputStreamReader(is));
+		return temp;
+	}
+
+}
