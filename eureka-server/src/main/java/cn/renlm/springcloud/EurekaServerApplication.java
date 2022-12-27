6ed74a21d69513da58311651ccf355f76b012a9e
任黎明(10021235) <renlm-a@glodon.com>
2022-12-27 10:44:39 +0800

    up

--- eureka-server/src/main/java/cn/renlm/springcloud/EurekaServerApplication.java
+++ eureka-server/src/main/java/cn/renlm/springcloud/EurekaServerApplication.java
@@ -2,13 +2,7 @@ package cn.renlm.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
-import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.client.support.BasicAuthenticationInterceptor;
-import org.springframework.web.client.RestTemplate;
 
 /**
  * 启动类
@@ -22,15 +16,6 @@ public class EurekaServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServerApplication.class, args);
-	}
-
-	@Bean
-	@LoadBalanced
-	public RestTemplate restTemplate(SecurityProperties securityProperties) {
-		User user = securityProperties.getUser();
-		RestTemplate restTemplate = new RestTemplate();
-		restTemplate.getInterceptors().add(new BasicAuthenticationInterceptor(user.getName(), user.getPassword()));
-		return restTemplate;
 	}
 
 }
