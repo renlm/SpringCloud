bd14e7c7973c3712179cd8c2a0d11fb7b57f5ec6
任黎明(10021235) <renlm-a@glodon.com>
2022-12-28 10:14:11 +0800

    up

--- config-server/src/main/java/cn/renlm/springcloud/ConfigServerApplication.java
+++ config-server/src/main/java/cn/renlm/springcloud/ConfigServerApplication.java
@@ -2,6 +2,7 @@ package cn.renlm.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
@@ -11,6 +12,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
  *
  */
 @EnableConfigServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ConfigServerApplication {
 
