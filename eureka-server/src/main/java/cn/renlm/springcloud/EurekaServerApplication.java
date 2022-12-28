9423ccbb5714bdc856b37bc6ea8f4f94a986007d
任黎明(10021235) <renlm-a@glodon.com>
2022-12-28 20:33:41 +0800

    up

--- eureka-server/src/main/java/cn/renlm/springcloud/EurekaServerApplication.java
+++ eureka-server/src/main/java/cn/renlm/springcloud/EurekaServerApplication.java
@@ -3,7 +3,6 @@ package cn.renlm.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 /**
  * 启动类
@@ -13,7 +12,6 @@ import org.springframework.session.data.redis.config.annotation.web.http.EnableR
  */
 @EnableEurekaServer
 @SpringBootApplication
-@EnableRedisHttpSession(redisNamespace = "eurekaserver:session")
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
