dcfb96f89a5bc72cd78cb033edf08de934cb3aaa
任黎明(10021235) <renlm-a@glodon.com>
2023-01-04 09:52:38 +0800

    up

---
+++ common/src/main/java/cn/renlm/springcloud/seata/FeignSeataInterceptor.java
@@ -0,0 +1,24 @@
+package cn.renlm.springcloud.seata;
+
+import static io.seata.common.DefaultValues.DEFAULT_LOAD_BALANCE;
+import org.springframework.context.annotation.Configuration;
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import io.seata.core.context.RootContext;
+
+/**
+ * Seata 分布式事物传递
+ * 
+ * @author Renlm
+ *
+ */
+@Configuration(proxyBeanMethods = false)
+public class FeignSeataInterceptor implements RequestInterceptor {
+
+	@Override
+	public void apply(RequestTemplate requestTemplate) {
+		requestTemplate.header(DEFAULT_LOAD_BALANCE, RootContext.getXID());
+	}
+
+}
