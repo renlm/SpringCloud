1a89855c43828fd7ba543d7772df5734dc32fa60
任黎明(10021235) <renlm-a@glodon.com>
2023-01-10 22:16:24 +0800

    up

--- eureka-server/src/main/java/cn/renlm/springcloud/EurekaServerApplication.java
+++ eureka-server/src/main/java/cn/renlm/springcloud/EurekaServerApplication.java
@@ -2,10 +2,7 @@ package cn.renlm.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.context.annotation.Bean;
 
 /**
  * 启动类
@@ -19,11 +16,6 @@ public class EurekaServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServerApplication.class, args);
-	}
-
-	@Bean
-	ServletWebServerFactory servletWebServerFactory() {
-		return new TomcatServletWebServerFactory();
 	}
 
 }
